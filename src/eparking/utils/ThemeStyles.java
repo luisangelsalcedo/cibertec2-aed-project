fe2aa9cc21596d3e87962b5502ef7091f48f52fc
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-06-15 19:23:00 -0500

    feat: Agregar estilos del tema (colores y fuente)

---
+++ src/eparking/utils/ThemeStyles.java
@@ -0,0 +1,49 @@
+package eparking.utils;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.net.URL;
+
+public class ThemeStyles {
+	public static Color $primaryColor;
+	public static Color $secondaryColor;
+	public static Color $accent;
+	public static Color $accentLight;
+	public static Color $accentDark;
+	public static Color $white;
+	public static Color $gray;
+	public static Color $dangerColor;
+	public static Color $infoColor;
+	public static Color $successColor;
+	
+	public static URL favicon;
+	
+	public static Font xsFont;
+	public static Font smFont;
+	public static Font mdFont;
+	public static Font lgFont;
+	public static Font xlFont;	
+	public static Font xxlFont;	
+	
+	static {
+		favicon 		= ThemeStyles.class.getResource(RootData.sourcePath + "favicon.png");
+		
+		$primaryColor 	= new Color(45, 45, 127);
+		$secondaryColor = new Color(0, 0, 0);
+		$accent 		= new Color(135, 100, 184);
+		$accentLight	= new Color(175, 151, 207);
+		$accentDark		= new Color(90, 66, 122);
+		$white 			= new Color(255, 255, 255);
+		$gray 			= new Color(217, 217, 217);
+		$dangerColor 	= new Color(255, 141, 100);
+		$infoColor 		= new Color(155, 182, 0);
+		$successColor 	= new Color(116, 202, 101);
+		
+		xsFont = new Font("Tahoma", Font.PLAIN, 8);
+		smFont = new Font("Tahoma", Font.PLAIN, 10);
+		mdFont = new Font("Tahoma", Font.PLAIN, 12);
+		lgFont = new Font("Tahoma", Font.PLAIN, 16);
+		xlFont = new Font("Tahoma", Font.PLAIN, 20);
+		xxlFont = new Font("Tahoma", Font.BOLD, 22);
+	}
+}
