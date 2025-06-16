ac39a7573c03d5d79a49e49a56a098ff39572de4
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-06-16 03:46:37 -0500

    fix: Registrar el reseteo del numero de intentos a 0 cuando el logeo sea exitoso

--- src/eparking/controllers/AuthController.java
+++ src/eparking/controllers/AuthController.java
@@ -52,6 +52,7 @@ public class AuthController {
 		
 		userFound.loginAttempt = 0; //reset attempt
 		setLoggedUser(userFound);
+		userDao.updateUser(userFound);
 		setErrorMessage(null);
 		return true;
 	}
