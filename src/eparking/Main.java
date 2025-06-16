95ad45e4ace2e4d03257dc6230b4344abf05778f
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-06-15 19:37:31 -0500

    Punto de inicio de la aplicacion

---
+++ src/eparking/Main.java
@@ -0,0 +1,22 @@
+package eparking;
+
+import java.awt.EventQueue;
+
+import eparking.views.LoginView;
+
+public class Main {
+	
+	// Launch the application.
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginView frame = new LoginView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
