a5fbe3087c12d4c83b8bf6c94f8713995042656a
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-06-15 19:23:45 -0500

    feat: Agregar informacion global de la aplicacion

---
+++ src/eparking/utils/RootData.java
@@ -0,0 +1,13 @@
+package eparking.utils;
+
+public class RootData {
+	public final static String AppTitle;
+	public final static String sourcePath;
+	public final static String version;
+	
+	static {
+		AppTitle = "eParking";
+		sourcePath = "/assets/";
+		version = "1.0.0";
+	}
+}
