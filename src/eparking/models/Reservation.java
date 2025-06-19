4a218a5b5219b13ce11a7afa77b0720344ac80ee
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-06-19 14:10:23 -0500

    feat: T23 - Crear Reservation model (T22)

---
+++ src/eparking/models/Reservation.java
@@ -0,0 +1,68 @@
+package eparking.models;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import eparking.enums.ReservationStatus;
+
+public class Reservation {
+    private int id;
+    private int userId;
+    private int parkingId;
+    private ReservationStatus status;
+    private LocalDate creationDate;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    Reservation(int userId, int parkingId){
+        this.setUserId(userId);
+        this.setParkingId(parkingId);
+        status = ReservationStatus.PENDING;
+        creationDate = LocalDate.now();
+    }
+
+    // getters
+	public int getId() {
+		return id;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public int getParkingId() {
+		return parkingId;
+	}
+	public ReservationStatus getStatus() {
+		return status;
+	}
+	public LocalDate getCreationDate() {
+		return creationDate;
+	}
+	public LocalTime getStartTime() {
+		return startTime;
+	}
+	public LocalTime getEndTime() {
+		return endTime;
+	}
+
+	// setters
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public void setParkingId(int parkingId) {
+		this.parkingId = parkingId;
+	}
+	public void setStatus(ReservationStatus status) {
+		this.status = status;
+	}
+	public void setCreationDate(LocalDate creationDate) {
+		this.creationDate = creationDate;
+	}
+	public void setStartTime(LocalTime startTime) {
+		this.startTime = startTime;
+	}
+	public void setEndTime(LocalTime endTime) {
+		this.endTime = endTime;
+	}
+}
