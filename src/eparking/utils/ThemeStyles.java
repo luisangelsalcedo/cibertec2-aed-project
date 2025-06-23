5436f654d9de9e87727318b88c231ee68eab0bc0
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-06-23 08:22:11 -0500

    feat: agregar nuevo color primaryColorLight

--- src/eparking/utils/ThemeStyles.java
+++ src/eparking/utils/ThemeStyles.java
@@ -6,6 +6,7 @@ import java.net.URL;
 
 public class ThemeStyles {
 	public static Color $primaryColor;
+	public static Color $primaryColorLight;
 	public static Color $secondaryColor;
 	public static Color $accent;
 	public static Color $accentLight;
@@ -28,16 +29,17 @@ public class ThemeStyles {
 	static {
 		favicon 		= ThemeStyles.class.getResource(RootData.sourcePath + "favicon.png");
 		
-		$primaryColor 	= new Color(45, 45, 127);
-		$secondaryColor = new Color(0, 0, 0);
-		$accent 		= new Color(135, 100, 184);
-		$accentLight	= new Color(175, 151, 207);
-		$accentDark		= new Color(90, 66, 122);
-		$white 			= new Color(255, 255, 255);
-		$gray 			= new Color(217, 217, 217);
-		$dangerColor 	= new Color(255, 141, 100);
-		$infoColor 		= new Color(155, 182, 0);
-		$successColor 	= new Color(116, 202, 101);
+		$primaryColor 		= new Color(45, 45, 127);
+		$primaryColorLight 	= new Color(115, 115, 169);
+		$secondaryColor 	= new Color(0, 0, 0);
+		$accent 			= new Color(135, 100, 184);
+		$accentLight		= new Color(175, 151, 207);
+		$accentDark			= new Color(90, 66, 122);
+		$white 				= new Color(255, 255, 255);
+		$gray 				= new Color(217, 217, 217);
+		$dangerColor 		= new Color(230, 141, 141);
+		$infoColor 			= new Color(155, 182, 0);
+		$successColor 		= new Color(116, 202, 101);
 		
 		xsFont = new Font("Tahoma", Font.PLAIN, 8);
 		smFont = new Font("Tahoma", Font.PLAIN, 10);
