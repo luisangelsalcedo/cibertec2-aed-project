96eed5889e16f8a3bf8fc3fd61181e45419e4eff
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-07-05 20:55:35 -0500

    chore: formateo de codigo en la clase user

--- src/eparking/models/User.java
+++ src/eparking/models/User.java
@@ -22,8 +22,7 @@ public class User {
 	
 	public static User createAdmin(String userName, String password) {
 		return new User(Permission.ADMIN.toString(), userName, password, Permission.ADMIN);
-	}
-	
+	}	
 	public static User createUser(String userName, String password) {
 		return new User(Permission.USER.toString(), userName, password, Permission.USER);
 	}
@@ -31,28 +30,22 @@ public class User {
 	// getters
 	public int getId() {
 		return id;
-	}
-	
+	}	
 	public String getUserName() {
 		return userName;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public Permission getPermission() {
 		return permission;
-	}
-	
+	}	
 	public int getLoginAttempt() {
         return loginAttempt;
     }
-
 	public String getName() {
 		return name;
-	}
-	
+	}	
 	public boolean isUserLock() {
 		return userLock;
 	}
@@ -60,28 +53,22 @@ public class User {
 	// setters
 	public void setId(int id) {
 		this.id = id;
-	}
-	
+	}	
 	public void setName(String name) {
 		this.name = name;
-	}	
-
+	}
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 	public void setPermission(Permission permission) {
 		this.permission = permission;
-	}
-	
+	}	
 	public void setLoginAttempt(int loginAttempt) {
         this.loginAttempt = loginAttempt;
     }
-
 	public void setUserLock(boolean userLock) {
 		this.userLock = userLock;
 	}
